1682be3b25b789de2270abea3271e7a471a7b0aa
Sofie Hemmingsen <sofi413c@stud.kea.dk>
2023-02-16 14:36:09 +0100

    second commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    color: {
+      "primary": '#5B5E64',
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
-};
+}; //fortæller hvordan tailwind skal opfører sig.//
+
+
