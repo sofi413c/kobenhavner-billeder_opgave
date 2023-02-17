680f0ea3aa7875661dc98a1372f43d7ed89b2b36
Sofie Hemmingsen <sofi413c@stud.kea.dk>
2023-02-17 11:33:14 +0100

    justeringer img og magin

--- src/darkmode.js
+++ src/darkmode.js
@@ -1,13 +1,15 @@
-//referencer
-// const root = document.documentElement; svarer til det samme som den nederunder html
-const root = document.querySelector("html");
-const btn = document.querySelector("#toggle-btn");
+//hele scriptet bliver exporteret med export variablen
+export function darkMode () {
+  //referencer
+  // const root = document.documentElement; svarer til det samme som den nederunder html
+  const root = document.querySelector("html");
+  const btn = document.querySelector("#toggle-btn");
 
-//toggle funktionalitet
-function toggleTheme() {
-  root.classList.toggle("dark"); //hiver fat i html root referencen
+  //toggle funktionalitet
+  function toggleTheme() {
+    root.classList.toggle("dark"); //hiver fat i html root referencen
+  }
+
+  btn.addEventListener("click", toggleTheme);
+
 }
-
-btn.addEventListener("click", toggleTheme);
-
-
--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 import "./style.css";
+import { darkMode } from "./darkmode.js";
+
+darkMode();
+//fra darkmode.js vil vi gerne importerer darkMode, som kommer fra funktionsnavnet inde i darkmode.js. På den måde tager vi alt indmaden fra darkmode.js og smider den herind. På den måde kan vi nøjes med kun at have et script tag i HTML. Nemlig main.js
